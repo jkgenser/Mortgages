6529ae9a999fc8e87211422f341ce247d816ed1a
jkgenser <jerrygenser@gmail.com>
2015-10-04 14:58:01 -0400

    commit

--- leaflet_script.js
+++ leaflet_script.js
@@ -93,7 +93,7 @@ var percentBuilder = function(str) {
 info.update = function (props) {
     this._div.innerHTML = '<h4>How much harder is it for black people to get loans?</h4>' +
           (props ?
-        '<b>' + props.name + '</b><br />' + props.black_coeff.toFixed(2) + ' = black loan difficulty index' +
+        '<b>' + props.name + '</b><br />' + props.black_coeff.toFixed(2) + ' = black loan difficulty index (white odds:black odds)'+ 
         '<br>' + addCommas(props.tot_apps.toString()) + ' mortgage loan applications in the state in 2014.' + 
         '<br>' + percentBuilder(props.black_share.toString()) + ' share of applicants were black.'
         : 'Move your pointer over a state');
